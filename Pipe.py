12369811f5143c772a4b56ce69a75675b51cb78c
mynamenn <nguleychun@gmail.com>
2020-05-10 19:00:04 +0800

    Edited pipe and player

--- Pipe.py
+++ Pipe.py
@@ -1,9 +1,26 @@
 ''' '''
+import random
 
 class Pipe:
 
-    def __init__(self, position, velocity):
+    def __init__(self, velocityX, GAME_SPRITES, SCREENHEIGHT):
 
-        self.position = position
+        self.velocityX = velocityX
 
-        self.velocity = velocity
+        self.GAME_SPRITES = GAME_SPRITES
+
+        self.SCREENHEIGHT = SCREENHEIGHT
+
+
+    def getRandomPipe(self):
+        pipeHeight = self.GAME_SPRITES['pipe'][0].get_height()
+        offset = self.SCREENHEIGHT / 3
+        y2 = offset + random.randrange(0, int(self.SCREENHEIGHT - self.GAME_SPRITES['base'].get_height() - 1.2 * offset))
+        pipeX = self.SCREENHEIGHT + 10
+        y1 = pipeHeight - y2 + offset
+        pipe = [
+            {'x': pipeX, 'y': -y1},  # upper Pipe
+            {'x': pipeX, 'y': y2}  # lower Pipe
+        ]
+        return pipe
+
--- Player.py
+++ Player.py
@@ -2,8 +2,22 @@
 
 class Player:
 
-    def __init__(self, position, velocity):
+    def __init__(self, positionX, positionY, velocityY, maxVelocityY, minVelocityY, accY, BIRD_IMGS, flappedAccVel, flapped):
 
-        self.position = position
+        self.positionX = positionX
 
-        self.velocity = velocity
+        self.positionY = positionY
+
+        self.velocityY = velocityY
+
+        self.maxVelocityY = maxVelocityY
+
+        self.minVelocityY = minVelocityY
+
+        self.accY = accY
+
+        self.BIRD_IMGS = BIRD_IMGS
+
+        self.flapped = flapped
+
+        self.flappedAccVel = flappedAccVel #velocity while flapping
